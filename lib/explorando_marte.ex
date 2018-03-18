bea22e6bfa9af4249b006aa691e8cb4964073cc8
Gustavo Santos <gustavosms@localhost.localdomain>
2018-03-18 15:07:21 -0300

    restringindo movimentos pelo planalto

--- lib/explorando_marte.ex
+++ lib/explorando_marte.ex
@@ -1,31 +1,38 @@
 defmodule ExplorandoMarte do
   
+  @doc """
+    Processa a nova posição de uma sonda dentro do planalto, após a execução das instruções
+  """
+  @callback processa_sonda(planalto :: %Planalto{}, posicao_inicial :: %PosicaoSonda{}, instrucoes :: String) :: %PosicaoSonda{}
   def processa_sonda(planalto, posicao_inicial, instrucoes) do
-      processa_instrucoes(posicao_inicial, String.graphemes(instrucoes))
+      processa_instrucoes(posicao_inicial, planalto, String.graphemes(instrucoes))
   end
 
-  defp processa_instrucoes(posicao = %PosicaoSonda{}, [instrucao | rest]) do
-    posicao |> processa_instrucao(instrucao) |> processa_instrucoes(rest)
+  defp processa_instrucoes(posicao = %PosicaoSonda{}, planalto = %Planalto{}, [instrucao | rest]) do
+    posicao |> processa_instrucao(planalto, instrucao) |> processa_instrucoes(planalto, rest)
   end
 
-  defp processa_instrucoes(posicao, []) do
+  defp processa_instrucoes(posicao, _, []) do
     posicao
   end
 
-  defp processa_instrucao(posicao, "M") do
+  defp processa_instrucao(posicao, planalto, "M") do
+    funIncremento = {&(&1 > &2), &(&1 + 1)}
+    funDecremento = {&(&1 < &2), &(&1 - 1)}
+
     case posicao.orientacao do
       "N" -> 
-        %{posicao | y: posicao.y + 1}
+        %{posicao | y: calcula_nova_posicao(planalto.max_y, posicao.y, funIncremento)}
       "S" -> 
-        %{posicao | y: posicao.y - 1}
+        %{posicao | y: calcula_nova_posicao(planalto.min_y, posicao.y, funDecremento)}
       "E" -> 
-        %{posicao | x: posicao.x + 1}
+        %{posicao | x: calcula_nova_posicao(planalto.max_x, posicao.x, funIncremento)}
       "W" -> 
-        %{posicao | x: posicao.x - 1}
+        %{posicao | x: calcula_nova_posicao(planalto.min_x, posicao.x, funDecremento)}
     end
   end
 
-  defp processa_instrucao(posicao, "L") do
+  defp processa_instrucao(posicao, _, "L") do
     case posicao.orientacao do
       "N" -> 
         %{posicao | orientacao: "W"}
@@ -38,7 +45,7 @@ defmodule ExplorandoMarte do
     end
   end
 
-  defp processa_instrucao(posicao, "R") do
+  defp processa_instrucao(posicao, _, "R") do
     case posicao.orientacao do
       "N" -> 
         %{posicao | orientacao: "E"}
@@ -48,6 +55,14 @@ defmodule ExplorandoMarte do
         %{posicao | orientacao: "S"}
       "W" -> 
         %{posicao | orientacao: "N"}
+    end
+  end
+
+  defp calcula_nova_posicao(limite_posicao, posicao_atual, {funComp, funNext}) do
+    if funComp.(funNext.(posicao_atual), limite_posicao) do
+      posicao_atual
+    else
+      funNext.(posicao_atual)
     end
   end
 
--- lib/model/planalto.ex
+++ lib/model/planalto.ex
@@ -1,3 +1,3 @@
 defmodule Planalto do
-    defstruct max_x: 0, max_y: 0
+    defstruct max_x: 0, max_y: 0, min_x: 0, min_y: 0
 end
